f761cf9fde346d6736e946f754514d65d0e7fc12
ORZGG <ab167943@gmail.com>
2018-02-18 22:41:00 +0800

    Add chapter11 code

---
+++ chapter11/include/texturemanager.h
@@ -0,0 +1,36 @@
+#ifndef TEXTUREMANAGER_H
+#define TEXTUREMANAGER_H
+
+// cpp native header
+#include <string>
+
+// c-style header
+#include <SDL_Utility.h>
+
+class TextureManager
+{
+public:
+  // ctor & dtor
+  TextureManager( const std::string& p, SDL_PixelFormat* sf, SDL_Renderer* r ) ;
+  TextureManager( SDL_Surface* const src, SDL_Renderer* r ) ;
+  virtual ~TextureManager();
+  // pubilc method
+  void Free() ;
+  void Render() ;
+  void Render( const int x, const int y, SDL_Rect* src, int scale ) ;
+  // info getter
+  int GetWidth() { return mWidth ; }
+  int GetHeight() { return mHeight ; }
+  // END OF DECORATION
+protected:
+
+private:
+  SDL_Texture* mTexture = NULL ;
+  SDL_Renderer* render = NULL ;
+  uint32_t mTextureFormat = 0 ;
+  int accessMode = SDL_TEXTUREACCESS_STATIC ;
+  int mWidth = 0 ;
+  int mHeight = 0 ;
+};
+
+#endif // TEXTUREMANAGER_H
